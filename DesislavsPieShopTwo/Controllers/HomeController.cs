448cd05b562d296c4bc2a05f9859e43c3dc53aa7
unknown <petrov_desislav@abv.bg>
2018-02-17 06:15:57 +0200

    created the HomeController, Index view, and styling the view with Bootstrap

---
+++ DesislavsPieShopTwo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DesislavsPieShopTwo.Models;
+using System.Linq;
+using DesislavsPieShopTwo.ViewModels;
+
+namespace DesislavsPieShopTwo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Desislav's Pie Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ DesislavsPieShopTwo/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DesislavsPieShopTwo.Models;
+using System.Collections.Generic;
+
+namespace DesislavsPieShopTwo.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
